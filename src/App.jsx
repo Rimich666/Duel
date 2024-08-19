f5c40dfcdfd31a196e107dc6d33b99bf0ae69dbc
Rimich666 <89179699365@mail.ru>
2024-08-19 10:09:59 +0300

    init: init duel

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Field from "./field.jsx";
+
+function App() {
+  return (
+    <Field/>
+  )
+}
+
+export default App
---
+++ src/field.jsx
@@ -0,0 +1,10 @@
+export default function Field() {
+  return (
+    <div>
+      <div>
+        <button type="button">Start</button>
+      </div>
+      <canvas id="canvas" width="400" height="600"></canvas>
+    </div>
+  );
+}
